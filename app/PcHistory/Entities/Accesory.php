d0802150a3309d893a9c965a67bf0e055ba07063
Snheidert <smalbach24@gmail.com>
2014-06-16 22:15:04 -0500

    All

--- app/PcHistory/Entities/Accesory.php
+++ app/PcHistory/Entities/Accesory.php
@@ -10,6 +10,18 @@ namespace PcHistory\Entities;
 
 
 class Accesory  extends \Eloquent {
+    protected $fillable = array(
+
+        'name',
+        'description',
+        'capacity',
+        'photo',
+        'unity',
+        'state',
+
+    );
+
+
 
     public function accesoriesxpc(){
         return $this->hasMany('PcHistory\Entities\AccesoriesxPc');
